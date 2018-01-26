259542fb838e448c11a789228fe64a824d49f051
KADFWJ <930135305@qq.com>
2018-01-26 14:08:14 +0800

    0.0.4

--- LMHomeModule/LMHomeModule/ViewController/LMHomeVC.m
+++ LMHomeModule/LMHomeModule/ViewController/LMHomeVC.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor blackColor];
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 -(void)registerViewController:(UIViewController *)vc title:(NSString *)title iconName:(NSString *)iconName{
